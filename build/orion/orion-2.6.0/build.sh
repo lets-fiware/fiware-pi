2f980ffa94208911299fd33f71ca204058cf226c
Kazuhito Suda <kazuhito@fisuda.jp>
2021-03-28 11:03:41 +0900

    Hardening Orion 2.6.0

---
+++ build/orion/orion-2.6.0/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/telefonicaid/fiware-orion.git
+cd fiware-orion/docker
+docker build --build-arg IMAGE_TAG=centos7 --build-arg GIT_REV_ORION=2.6.0 -t orion:2.6.0 .
